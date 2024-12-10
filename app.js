f57e2346da75d78eb4bcbd16f4a502ffb6591b44
laxman4421w <147487577+laxman4421w@users.noreply.github.com>
2024-12-11 03:19:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 
+const express = require('express');
+const app = express();
+const PORT = 7000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, Docker!');
+});
+
+app.listen(PORT, () => {
+    console.log('Server is running on port 7000');
+});
